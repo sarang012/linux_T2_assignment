bb6e15d43ba2f849940289cee5ea2d49c581244e
sarang012 <61492794+sarang012@users.noreply.github.com>
2020-04-10 11:53:49 +0530

    Delete internet.sh

--- internet.sh
+++
@@ -1,7 +0,0 @@
-nc -z 8.8.8.8 53           # will return 0 if internet working
-online=$?                  # variable storing the return value
-if [ $online -eq 0 ]; then
-    echo "Internet is UP"
-else
-    echo "Internet is DOWN"
-fi
